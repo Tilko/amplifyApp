3e7ac56b0efaf136e6d5fb70b9f529ef88bc5093
gmart <martinetti.gregoire@gmail.com>
2020-07-27 14:05:45 +0200

    add ";"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
-
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -20,14 +19,15 @@ function App() {
     const apiData = await API.graphql({ query: listNotes });
     setNotes(apiData.data.listNotes.items);
   }
+
   async function createNote() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
     setNotes([...notes, formData]);
     setFormData(initialFormState);
   }
+
   async function deleteNote({ id }) {
-    console.log("deleted'id:" + id)
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
     await API.graphql({ query: deleteNoteMutation, variables: { input: { id } } });
